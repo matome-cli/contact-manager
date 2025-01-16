da74a99ac6ff70629e3e410a9faadba05fe2690f
matome-cli <matomemjtome@gmail.com>
2025-01-16 15:54:47 +0200

    removed dialog component

--- src/components/app/Contacts/ContactsLayout.tsx
+++ src/components/app/Contacts/ContactsLayout.tsx
@@ -3,7 +3,7 @@ import ContactCard from "./ContactCard";
 
 type ContactLayoutProps = {
   contacts: Contact[];
-  dispatch: React.Dispatch<ContactDispatch> // prop drilling 1 lvl
+  dispatch: React.Dispatch<ContactDispatch>; // prop drilling 1 lvl
 };
 
 const ContactLayout: React.FC<ContactLayoutProps> = ({ contacts, dispatch }) => {
@@ -12,7 +12,6 @@ const ContactLayout: React.FC<ContactLayoutProps> = ({ contacts, dispatch }) => 
       <ContactCard key={i} name={contact.name} cell={contact.cell} dispatch={dispatch} />
     )
   );
-
 
   return (
     <section className="w-full max-h-[80%] h-fit flex flex-col justify-start items-center space-y-4 p-2">
