e1c61438ff47874bf45711e0d9749a445f1cf3cb
matome-cli <matomemjtome@gmail.com>
2025-01-20 16:56:06 +0200

    used tailwind in AppHeader.tsx

--- src/components/app/AppHeader.tsx
+++ src/components/app/AppHeader.tsx
@@ -3,12 +3,11 @@ type AppHeaderProps = {
 };
 
 const AppHeader: React.FC<AppHeaderProps> = ({ shrink }) => {
-  let dynamicWidth: string = shrink ? "83.333333%" : "100%";
-
   return (
     <header
-      className="h-[50px] bg-transparent absolute top-0 right-0 pl-3 pt-1 font-semibold font-serif text-2xl md:text-4xl"
-      style={{ width: dynamicWidth }}
+      className={`h-[50px] bg-transparent absolute top-0 right-0 pl-3 pt-1 font-semibold font-serif text-2xl md:text-3xl ${
+        shrink ? "lg:w-[80%]" : "w-full"
+      }`}
     >
       Contact manager
     </header>
