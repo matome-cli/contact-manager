152412c80d74543c2b1eff7e483fe38d1e7f46b9
matome-cli <matomemjtome@gmail.com>
2025-01-20 15:53:45 +0200

    AppHeader.tsx text is larger on big screens

--- src/components/app/AppHeader.tsx
+++ src/components/app/AppHeader.tsx
@@ -7,7 +7,7 @@ const AppHeader: React.FC<AppHeaderProps> = ({ shrink }) => {
 
   return (
     <header
-      className="h-[50px] bg-transparent absolute top-0 right-0 pl-3 pt-1 font-semibold font-serif text-2xl"
+      className="h-[50px] bg-transparent absolute top-0 right-0 pl-3 pt-1 font-semibold font-serif text-2xl md:text-4xl"
       style={{ width: dynamicWidth }}
     >
       Contact manager
