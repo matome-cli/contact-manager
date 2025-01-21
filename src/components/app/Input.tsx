b2281a99b7215d1b9405fb15aa8a088ec6a89368
matome-cli <matomemjtome@gmail.com>
2025-01-21 13:43:06 +0200

    made changes to Input.tsx

--- src/components/app/Input.tsx
+++ src/components/app/Input.tsx
@@ -14,7 +14,7 @@ const Input: React.FC<InputProps> = ({ setContactSearch }) => {
     <input
       type="text"
       placeholder="Search contact name"
-      className="absolute top-32 left-[50%] -translate-x-[50%] w-5/6 p-2 outline-none rounded-md"
+      className="absolute top-32 left-[50%] -translate-x-[50%] w-5/6 p-2 outline-none rounded-md md:left-[53%] md:w-[80%]"
       onChange={handleChange}
     />
   );
