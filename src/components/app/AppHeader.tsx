778fbc86d42e980ed29376dcd573fd007e20b934
matome-cli <matomemjtome@gmail.com>
2025-01-21 13:34:30 +0200

    got rid of shrink prop and use tailwind media queries for AppHeader.jsx

--- src/components/app/AppHeader.tsx
+++ src/components/app/AppHeader.tsx
@@ -1,14 +1,6 @@
-type AppHeaderProps = {
-  shrink: boolean;
-};
-
-const AppHeader: React.FC<AppHeaderProps> = ({ shrink }) => {
+const AppHeader: React.FC = () => {
   return (
-    <header
-      className={`h-[50px] bg-transparent absolute top-0 right-0 pl-3 pt-1 font-semibold font-serif text-2xl md:text-3xl ${
-        shrink ? "lg:w-[80%]" : "w-full"
-      }`}
-    >
+    <header className="h-[50px] bg-transparent absolute top-0 right-0 pl-3 pt-1 font-semibold font-serif text-2xl md:text-3xl w-full md:w-4/5">
       Contact manager
     </header>
   );
