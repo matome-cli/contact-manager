04be700f885dc7627b754c57f3a6e1745afa5d7f
matome-cli <matomemjtome@gmail.com>
2025-01-21 13:38:29 +0200

    changed width of App.Header

--- src/components/app/AppHeader.tsx
+++ src/components/app/AppHeader.tsx
@@ -1,6 +1,6 @@
 const AppHeader: React.FC = () => {
   return (
-    <header className="h-[50px] bg-transparent absolute top-0 right-0 pl-3 pt-1 font-semibold font-serif text-2xl md:text-3xl w-full md:w-4/5">
+    <header className="h-[50px] bg-transparent absolute top-0 right-0 pl-3 pt-1 font-semibold font-serif text-2xl md:text-3xl w-full md:w-[90%]">
       Contact manager
     </header>
   );
