db2a2a5b587d4b6d32eb280859740873466e0dae
matome-cli <matomemjtome@gmail.com>
2025-01-23 20:05:30 +0200

    error 404

--- src/components/PageNotFound.tsx
+++ src/components/PageNotFound.tsx
@@ -1,20 +1,32 @@
 import { Link } from "react-router-dom";
+import AppHeader from "./app/AppHeader";
 
 const PageNotFound: React.FC = () => {
   return (
-    <main className="w-full h-dvh bg-gradient-to-t from-orange-600 to-white relative">
-      <header className="w-full absolute top-0 left-0 pl-4 pt-4 font-semibold font-serif text-2xl">
-        Contact manager
-      </header>
+    <main className="w-full h-dvh flex flex-col justify-center items-center gap-9 bg-gradient-to-t from-orange-600 to-white relative">
+      {/* out of normal flow */}
+      <AppHeader fullWidth={true} />
 
-      <h1 className="text-4xl">Oops!</h1>
+      <h1 className="text-6xl">Oops!</h1>
       <p>We can't seem to find the page you are looking for</p>
       {/* this should not take user back to login but the actual app */}
 
-      <div>
-        <Link to="/">Login</Link>
-        <Link to="/create-account">Signup</Link>
-        <Link to="/contacts">Contacts</Link>
+      <div className="space-x-8">
+        <Link className="text-orange-700 ml-2 lg:hover:underline decoration-wavy " to="/">
+          Login
+        </Link>
+        <Link
+          className="text-orange-700 ml-2 lg:hover:underline decoration-wavy "
+          to="/create-account"
+        >
+          Signup
+        </Link>
+        <Link
+          className="text-orange-700 ml-2 lg:hover:underline decoration-wavy "
+          to="/manager"
+        >
+          Contacts
+        </Link>
       </div>
     </main>
   );
