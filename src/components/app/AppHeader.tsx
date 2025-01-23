104591c471ecbcd044b5de6df617717b6999d4d5
matome-cli <matomemjtome@gmail.com>
2025-01-23 14:45:07 +0200

    positioning of app header and input

--- src/components/app/AppHeader.tsx
+++ src/components/app/AppHeader.tsx
@@ -13,7 +13,7 @@ const AppHeader: React.FC<AppHeaderProps> = ({ fullWidth }) => {
 
   // when fullWidth is undefined
   return (
-    <header className="h-[50px] bg-transparent absolute top-0 right-0 pl-3 pt-1 font-semibold font-serif text-2xl md:text-3xl w-full md:w-[90%]">
+    <header className="h-[50px] bg-transparent absolute top-0 right-0 pl-3 pt-1 font-semibold font-serif text-2xl md:text-3xl w-full md:w-[87%]">
       Contact manager
     </header>
   );
