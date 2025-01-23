3f9217ddd86c80305cc242e5262cba4e86a9b9b8
matome-cli <matomemjtome@gmail.com>
2025-01-23 14:52:52 +0200

    more concise and readable AppHeader.tsx code

--- src/components/app/AppHeader.tsx
+++ src/components/app/AppHeader.tsx
@@ -2,18 +2,13 @@ type AppHeaderProps = {
   fullWidth?: boolean;
 };
 
-const AppHeader: React.FC<AppHeaderProps> = ({ fullWidth }) => {
-  if (fullWidth) {
-    return (
-      <header className="h-[50px] bg-transparent absolute top-0 right-0 pl-3 pt-1 font-semibold font-serif text-2xl md:text-3xl w-full">
-        Contact manager
-      </header>
-    );
-  }
-
-  // when fullWidth is undefined
+const AppHeader: React.FC<AppHeaderProps> = ({ fullWidth = false }) => {
   return (
-    <header className="h-[50px] bg-transparent absolute top-0 right-0 pl-3 pt-1 font-semibold font-serif text-2xl md:text-3xl w-full md:w-[87%]">
+    <header
+      className={`h-[50px] bg-transparent absolute top-0 right-0 pl-3 pt-1 font-semibold font-serif text-2xl md:text-3xl w-full ${
+        !fullWidth ? "md:w-[87%]" : ""
+      }`}
+    >
       Contact manager
     </header>
   );
